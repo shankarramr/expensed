f46f53a89631b056e897b9f2a94b251c14cb0768
Shankar Ram <shankar.ram@aspiresys.com>
2019-07-31 10:08:22 +0300

    Moved theme service form shared to settings -> services -> theme

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ThemeService } from '../shared/theme.service';
+import { ThemeService } from './services/theme/theme.service';
 
 @Component({
   selector: 'app-settings',
